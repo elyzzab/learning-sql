d2d8ac8a756919bfd879b48922312670da2667b5
Elyzza B <45059345+elyzzab@users.noreply.github.com>
2022-10-03 00:43:01 -0500

    Update datalemur-challenges.sql

--- datalemur-challenges.sql
+++ datalemur-challenges.sql
@@ -8,3 +8,20 @@ FROM candidates
 WHERE skill IN ('Python','Tableau','PostgreSQL')
 GROUP BY candidate_id
 HAVING COUNT(skill) = 3;
+
+-- "Duplicate Job Listings"
+/* I was to find duplicate job entries for a job board (shared same job title, description, and company_id).
+I failed the first time but got it the second time, and my query varies quite a bit from the official solution.
+I think I did "too much heavy lifting" in the CTE I created; AKA, if someone were to try and re-create my work,
+they wouldn't be able to see my train of thought. Will try to be more thorough with my statements next time
+(as if I were to preview the query and question again after a long SQL-free vacation). */
+WITH filtered_jobs AS (
+    SELECT COUNT(DISTINCT company_id) AS dup_job_count
+    FROM job_listings
+    GROUP BY company_id
+    HAVING COUNT(company_id) >= 2
+      AND COUNT(title) >= 2
+      AND COUNT(description) >= 2
+)
+SELECT SUM(dup_job_count)
+FROM filtered_jobs;
